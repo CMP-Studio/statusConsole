5e3a47718016bb95d001a7d6620794304f13242a
drew <mcdermotta@carnegiemuseums.org>
2016-10-14 13:19:15 -0400

    import

--- status/views.py
+++ status/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from updateProject import updateProjects, pingProject
 from models import Project
 from django.http import JsonResponse
