d94fd6ddf7412c23ec9f944ccc6b21823bb6e2ef
drew <mcdermotta@carnegiemuseums.org>
2016-10-14 14:33:10 -0400

    import tasks

--- statusConsole/settings.py
+++ statusConsole/settings.py
@@ -14,7 +14,7 @@ import os
 from datetime import timedelta
 from secrets import secrets
 
-from status.tasks import runUpdate
+from status import tasks
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
