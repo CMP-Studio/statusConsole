cc358b2b6ba87864af589ac4f8f2071655e50459
drew <mcdermotta@carnegiemuseums.org>
2016-10-14 14:29:54 -0400

    importing runUpdate

--- statusConsole/settings.py
+++ statusConsole/settings.py
@@ -14,6 +14,8 @@ import os
 from datetime import timedelta
 from secrets import secrets
 
+from status.tasks import runUpdate
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
